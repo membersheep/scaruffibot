c3e46ab372cb98f88db01c26c0b586d2b8f056b0
Alessandro Maroso <alessandro.maroso@iks.it>
2016-09-14 17:28:05 +0200

    Updated start message.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.SERVER_PORT = process.env.PORT || 3000;
 config.WEBHOOK_BASE_URL = process.env.WEBHOOK_BASE_URL;
 config.WEBHOOK_PATH = process.env.WEBHOOK_PATH || '/telegramBot';
 config.TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
-config.START_MESSAGE = "Use the /search command to search anything in scaruffi.com. The search query must be separated from the command (e.g. '/search Karl Marx'). Since the bot is running in a Heroku free server it goes to sleep when it's not being used and you can experience some delay before receiving a response for your command. This bot is open source and it's been developed by github.com/membersheep.";
+config.START_MESSAGE = "Use the /search command to search anything in scaruffi.com. The search query must be separated from the command (e.g. '/search Karl Marx'). Use the /quote command to post a random quote by Piero. You can use the inline bot by writing the bot username (@scaruffibot) and then a search query. If you write 'quote' as search query a random quote will be gathered. Since the bot is running in a Heroku free server it goes to sleep when it's not being used and you can experience some delay before receiving a response for your command. This bot is open source and it's been developed by github.com/membersheep.";
 config.ERROR_MESSAGE_EMPTY_QUERY = "Please write a non empty search query after the command. Read /start for further instructions.";
 config.ERROR_MESSAGE_EMPTY_RESP = "Sorry, nothing found.";
 
